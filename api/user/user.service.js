1e111c4da1cca3dd4c579239526d35dfbf935e78
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-01-25 23:36:11 +0200

    login route completed for the user

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import { User } from "./user.model.js";
+import jwt from "jsonwebtoken";
 
 // register new user
 
@@ -24,4 +25,34 @@ export const registerUser = async (req, res) => {
 
   // send response
   return res.status(200).send({ message: "user registered successfully!" });
+};
+
+// login user
+
+export const loginUser = async (req, res) => {
+  // get login credentials from req.body
+  const loginCredentials = req.body;
+  // check if user exists
+  const user = await User.findOne({ email: loginCredentials.email });
+  if (!user) {
+    return res.status(404).send({ message: "user not found" });
+  }
+  // check if password matches
+  const passwordMatch = await bcrypt.compare(
+    loginCredentials.password,
+    user.password
+  );
+  if (!passwordMatch) {
+    return res.status(401).send({ message: "Invalid email or password" });
+  }
+  // remove password from user
+  user.password = undefined;
+  // create access token
+  const accessToken = jwt.sign(
+    { email: user.email },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "24h" }
+  );
+  // return appropriate response and send user and accessToken as user and token
+  return res.status(200).send({ user: user, token: accessToken });
 };
