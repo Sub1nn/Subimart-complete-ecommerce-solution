7d479661215bde98d09226f8baed66ae584b3686
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-01-25 23:34:17 +0200

    general constants used for the project

--- api/constants/general.constant.js
+++ api/constants/general.constant.js
@@ -1,2 +1,12 @@
 export const GenderOptions = ["male", "female", "other"];
 export const UserRoles = ["buyer", "seller"];
+export const productCategories = [
+  "electronics",
+  "clothing",
+  "grocery",
+  "cosmetics",
+  "toys",
+  "furniture",
+  "sports",
+  "stationery",
+];
