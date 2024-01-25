fcdd09849cc7526aa1ed342fa40ea3a01631d769
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-01-25 23:32:15 +0200

    productRoutes imported

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userRoutes from "./user/user.route.js";
 import { connectDb } from "./connect.db.js";
+import productRoutes from "./product/product.route.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ connectDb();
 
 // register routes
 app.use(userRoutes);
+app.use(productRoutes);
 
 // port
 const PORT = 3000;
