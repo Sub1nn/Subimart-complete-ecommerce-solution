309a672f425a422e1c340805217ebe39441f3c82
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-01-31 22:53:05 +0200

    add item to the cart post route completed

--- api/cart/cart.route.js
+++ api/cart/cart.route.js
@@ -11,7 +11,10 @@ router.post(
   "/cart/add/item",
   isBuyer,
   async (req, res, next) => {
+    // extract item from req.body
     const cartItem = req.body;
+
+    // validate item
     try {
       const validatedData = await addProductToCartValidationSchema.validate(
         cartItem
@@ -33,12 +36,43 @@ router.post(
     if (!isValidMongoId) {
       return res.status(400).send({ message: "Invalid mongo id" });
     }
+    // check if product exists
     const product = await Product.findOne({ _id: item.productId });
 
     // if not product, throw error
     if (!product) {
       return res.status(404).send({ message: "Product does not exist." });
     }
+    // ? check for product quantity
+    // if (product.quantity < item.orderedQuantity) {
+    //   return res
+    //  .status(400)
+    //  .send({ message: "Not enough product in stock." });
+    // }
+    // ? add item to cart
+    // const newItem = new Product(item);
+    // await newItem.save();
+    // res.status(201).send(newItem);
+
+    // if quantity not available throw error
+    if (item.orderedQuantity > product.quantity) {
+      return res.status(422).send({ message: "Product is outnumbered." });
+    }
+
+    // check if product is already added for this user
+    const cartItem = await Cart.findOne({
+      productId: item.productId,
+      buyerId: item.buyerId,
+    });
+    if (cartItem) {
+      return res.status(409).send({ message: "Product already in cart." });
+    }
+    // Create the cart, add item to the cart
+    await Cart.create(item);
+    // return response
+    return res
+      .status(201)
+      .send({ message: "Item is added to the cart successfully." });
   }
 );
 
