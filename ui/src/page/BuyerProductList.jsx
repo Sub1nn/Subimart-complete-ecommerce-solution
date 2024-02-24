70b54175ccb5dd887923b11aef3b528ba8ef2fb5
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-02-24 23:13:35 +0200

    feat: pagination added

--- ui/src/page/BuyerProductList.jsx
+++ ui/src/page/BuyerProductList.jsx
@@ -1,31 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import ProductCard from "../components/ProductCard";
 import { useQuery } from "react-query";
 import $axios from "../../lib/axios.instance";
-import { CircularProgress, Grid, Stack } from "@mui/material";
+import {
+  Box,
+  CircularProgress,
+  Grid,
+  Pagination,
+  Stack,
+  TextField,
+} from "@mui/material";
 import Loader from "../components/Loader";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
 const BuyerProductList = () => {
+  const [page, setPage] = useState(1);
+  const [searchText, setSearchText] = useState("");
   const { isLoading, isError, data, error } = useQuery({
-    queryKey: ["buyer-product-list"],
+    queryKey: ["buyer-product-list", page, searchText],
     queryFn: async () => {
       return await $axios.post("/product/buyer/list", {
-        page: 1,
-        limit: 5,
+        page: page,
+        limit: 4,
+        searchText: searchText,
       });
     },
   });
 
   const productData = data?.data?.products;
+  const totalPages = data?.data?.numberOfPages;
 
   if (isLoading) {
     return <Loader />;
   }
 
   return (
-    <div>
+    <>
+      <Box
+        sx={{ display: "grid", placeContent: "flex-end", m: "0 3rem 2rem 0" }}
+      >
+        <TextField
+          value={searchText}
+          label="Search"
+          variant="standard"
+          onChange={(event) => {
+            setSearchText(event?.target?.value || " ");
+          }}
+        />
+      </Box>
       <Grid container spacing={4} justifyContent="center">
         {productData.map((item) => {
           return (
@@ -35,7 +59,15 @@ const BuyerProductList = () => {
           );
         })}
       </Grid>
-    </div>
+      <Pagination
+        page={page}
+        count={totalPages}
+        color="secondary"
+        onChange={(event, value) => {
+          setPage(value);
+        }}
+      />
+    </>
   );
 };
 
