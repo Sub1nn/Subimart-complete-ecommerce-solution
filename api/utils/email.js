a8b803b3b6773c9c31237a069ab2c2fa9a84e1d7
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-03-05 23:32:23 +0200

    feat: nodemailer createTransport

---
+++ api/utils/email.js
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+
+const sendEmail = async (option) => {
+  // create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false, // You might need to set this to true in production
+    },
+  });
+
+  //define email options
+  const emailOptions = {
+    from: "Subimart support <support@subimart.com>",
+    to: option.email,
+    subject: option.subject,
+    text: option.message,
+  };
+
+  transporter.sendMail(emailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+    } else {
+      console.log("Email sent:", info.response);
+    }
+  });
+};
+
+export default sendEmail;
