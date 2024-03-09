ab8a6cfca83e3d8bb2b05b5b7ac11284437424da
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-03-09 22:51:00 +0200

    feat: product reducer added to the store

--- ui/src/store/index.js
+++ ui/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import snackbarReducer from "./slices/snackbar.slice";
+import productReducer from "./slices/productSlice";
 
 export default configureStore({
   reducer: {
     snackbar: snackbarReducer,
+    product: productReducer,
   },
 });
