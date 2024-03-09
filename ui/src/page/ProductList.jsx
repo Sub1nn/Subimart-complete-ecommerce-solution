38278c6d882ec225b7416f550d05e2663b4b1438
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-03-09 22:49:55 +0200

    feat : dispatch filter added on button click

--- ui/src/page/ProductList.jsx
+++ ui/src/page/ProductList.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 import SellerProductList from "./SellerProductList";
 import BuyerProductList from "./BuyerProductList";
-import { Box, useMediaQuery } from "@mui/material";
+import { Box, Button, Stack, TextField, useMediaQuery } from "@mui/material";
+import ProductFilter from "../components/ProductFilter";
+import SearchProduct from "../components/SearchProduct";
+import { useDispatch } from "react-redux";
+import { clearFilter } from "../store/slices/productSlice";
 
 const ProductList = () => {
+  const dispatch = useDispatch();
   const userRole = localStorage.getItem("role");
 
   return (
-    <Box
-      sx={{
-        padding: "1rem",
-      }}
-    >
-      {userRole === "seller" ? <SellerProductList /> : <BuyerProductList />}
-    </Box>
+    <>
+      <Stack direction={"row"} spacing={4}>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => {
+            dispatch(clearFilter());
+          }}
+        >
+          clear filter
+        </Button>
+        <ProductFilter />
+        <SearchProduct />
+      </Stack>
+      <Box
+        sx={{
+          padding: "1rem",
+        }}
+      >
+        {userRole === "seller" ? <SellerProductList /> : <BuyerProductList />}
+      </Box>
+    </>
   );
 };
 
