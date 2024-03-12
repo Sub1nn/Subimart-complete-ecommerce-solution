c51dca00c6a8304e58ecbd9e3ed297fbf0397743
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-03-12 22:28:41 +0200

    some needed practice

---
+++ api/cart/practice.js
@@ -0,0 +1,25 @@
+// const numbers = [1, 2, 3, 4, 5];
+// const mappedNumbers = numbers.map((num) => {
+//   if (num % 2 === 0) {
+//     return num * 2;
+//   }
+// });
+// const filteredNumbers = mappedNumbers.filter((num) => num !== undefined);
+// console.log(filteredNumbers);
+
+// const express = require('express');
+
+// const app = express();
+
+// app.get('/', (req) => {
+//     logService(req.query)
+// },
+// (req, res) => {
+//     res.send("hello");
+// }
+
+// )
+
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000");
+// })
