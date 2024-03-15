a45dda4e3fd2c62d1ceda815f5696b7677430d22
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-03-15 20:46:47 +0200

    feat: toggle page color theme

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -1,4 +1,6 @@
 import MenuIcon from "@mui/icons-material/Menu";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Badge, ThemeProvider, createTheme } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -13,12 +15,11 @@ import ListItemText from "@mui/material/ListItemText";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
-import { useNavigate } from "react-router-dom";
-import LogoutDialog from "./LogoutDialog";
-import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import { Badge } from "@mui/material";
 import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
 import $axios from "../../lib/axios.instance";
+import LogoutDialog from "./LogoutDialog";
+import ThemeToggle from "./ThemeToggle";
 
 const drawerWidth = 240;
 const navItems = [
@@ -39,7 +40,18 @@ const navItems = [
   },
 ];
 
+const lightTheme = createTheme();
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 const Header = (props) => {
+  const [theme, setTheme] = React.useState("light");
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
   const userRole = localStorage.getItem("role");
   const navigate = useNavigate();
   const { window } = props;
@@ -88,93 +100,92 @@ const Header = (props) => {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar component="nav" sx={{ backgroundColor: "#5D3587" }}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          >
-            Subi Mart
-          </Typography>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navItems.map((item) => (
-              <Button
-                key={item.id}
-                sx={{ color: "#fff" }}
-                onClick={() => {
-                  navigate(item.path);
-                }}
-              >
-                {item.name}
-              </Button>
-            ))}
-          </Box>
-          <Box
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar component="nav" sx={{ backgroundColor: "#5D3587" }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            >
+              Subi Mart
+            </Typography>
+            <ThemeToggle toggleTheme={toggleTheme} theme={theme} />
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              {navItems.map((item) => (
+                <Button
+                  key={item.id}
+                  sx={{ color: "#fff" }}
+                  onClick={() => {
+                    navigate(item.path);
+                  }}
+                >
+                  {item.name}
+                </Button>
+              ))}
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                gap: "1rem",
+                marginLeft: "3rem",
+              }}
+            >
+              {userRole === "buyer" && (
+                <IconButton
+                  size="large"
+                  onClick={() => {
+                    navigate("/cart");
+                  }}
+                >
+                  <Badge
+                    badgeContent={cartItemCount || null}
+                    color="primary"
+                    sx={{ cursor: "pointer" }}
+                  >
+                    <ShoppingCartOutlinedIcon sx={{ color: "white" }} />
+                  </Badge>
+                </IconButton>
+              )}
+              <LogoutDialog />
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <nav>
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true,
+            }}
             sx={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              gap: "1rem",
-              marginLeft: "3rem",
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
             }}
           >
-            {userRole === "buyer" && (
-              <IconButton
-                size="large"
-                onClick={() => {
-                  navigate("/cart");
-                }}
-              >
-                <Badge
-                  badgeContent={cartItemCount || null}
-                  color="primary"
-                  sx={{ cursor: "pointer" }}
-                >
-                  <ShoppingCartOutlinedIcon sx={{ color: "white" }} />
-                </Badge>
-              </IconButton>
-            )}
-            <LogoutDialog />
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-      {/* <Box component="main" sx={{ p: 3 }}>
-        <Toolbar />
-        <Typography></Typography>
-      </Box> */}
-    </Box>
+            {drawer}
+          </Drawer>
+        </nav>
+      </Box>
+    </ThemeProvider>
   );
 };
 
