8646965c4868066e550dcb654ad04a827bd2da10
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-03-15 21:23:41 +0200

    feat: useDebounce hook implemented in the component

--- ui/src/components/SearchProduct.jsx
+++ ui/src/components/SearchProduct.jsx
@@ -1,20 +1,32 @@
 import { FormControl, Input, InputAdornment } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import { useDispatch } from "react-redux";
 import { setSearchText } from "../store/slices/productSlice";
+import useDebounce from "../../Hooks/useDebounce";
 
 const SearchProduct = () => {
   const dispatch = useDispatch();
+  const [searchInput, setSearchInput] = useState("");
+
+  // Use debounce hook with 500ms delay
+  const debouncedSearchInput = useDebounce(searchInput, 500);
+
+  const handleInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  // Dispatch setSearchText with debounced value
+  React.useEffect(() => {
+    console.log(debouncedSearchInput);
+    dispatch(setSearchText(debouncedSearchInput));
+  }, [debouncedSearchInput, dispatch]);
 
   return (
     <FormControl variant="standard">
       <Input
-        onChange={(event) => {
-          const searchText = event?.target?.value;
-
-          dispatch(setSearchText(searchText));
-        }}
+        value={searchInput}
+        onChange={handleInputChange}
         startAdornment={
           <InputAdornment position="start">
             <SearchIcon />
