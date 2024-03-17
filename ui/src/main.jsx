ee1c9196756402e074c56209ca8ddbd3d8f72f81
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-03-17 23:14:53 +0200

    chore: general code cleanup

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,12 +1,12 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
 
+import { QueryClient, QueryClientProvider } from "react-query";
 import guestRoutes from "./routes/GuestRoutes";
 import mainRoutes from "./routes/MainRoutes";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider } from "react-redux";
 import store from "./store";
