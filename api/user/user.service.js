9a3b54c6fbaa37e206950401cf8849baa47bbd5e
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-03-20 23:53:29 +0200

    email verification auth checked

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import { User } from "./user.model.js";
 import jwt from "jsonwebtoken";
+import sendEmail from "../utils/email.js";
 
 // register new user
 
@@ -23,8 +24,26 @@ export const registerUser = async (req, res) => {
   // create user
   await User.create(newUser);
 
-  // send response
-  return res.status(200).send({ message: "user registered successfully!" });
+  // Send the verification link link to the user's email
+  const resetURL = `${req.protocol}://${req.get(
+    "host"
+  )}/verify-email/${encodeURIComponent(newUser.email)}`;
+  const message = `Click on the following link to verify your email: ${resetURL}`;
+
+  try {
+    await sendEmail({
+      email: newUser.email,
+      subject: "verify email",
+      message,
+    });
+
+    return res
+      .status(201)
+      .json({ message: "Verification link send to your Email" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 // login user
@@ -37,6 +56,30 @@ export const loginUser = async (req, res) => {
   if (!user) {
     return res.status(404).send({ message: "user not found" });
   }
+
+  // Check if user's email is verified
+  if (!user.verified) {
+    // If email not verified, resend the verification link
+    const resetURL = `${req.protocol}://${req.get(
+      "host"
+    )}/verify-email/${encodeURIComponent(loginCredentials.email)}`;
+    const message = `Click on the following link to verify your email: ${resetURL}`;
+
+    try {
+      await sendEmail({
+        email: user.email,
+        subject: "Verify your email",
+        message,
+      });
+      return res
+        .status(403)
+        .send({ message: "Email not verified. Verification link sent again." });
+    } catch (error) {
+      console.error("Error sending email:", error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
   // check if password matches
   const passwordMatch = await bcrypt.compare(
     loginCredentials.password,
