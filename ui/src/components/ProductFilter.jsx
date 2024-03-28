ba83d914af429161382968f529499775cd206967
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-03-28 22:56:42 +0200

    feat: price and category filter added

--- ui/src/components/ProductFilter.jsx
+++ ui/src/components/ProductFilter.jsx
@@ -1,15 +1,22 @@
-import * as React from "react";
+import {
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
-import { setCategory } from "../store/slices/productSlice";
-import PriceRangePicker from "./PriceRangePicker";
+import { Formik } from "formik";
+import * as React from "react";
+import { useDispatch } from "react-redux";
+import * as Yup from "yup";
+import { setCategory, setMaxPrice } from "../store/slices/productSlice";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -28,8 +35,6 @@ const categories = [
 
 const ProductFilter = () => {
   const dispatch = useDispatch();
-
-  const [selectedCategory, setSelectedCategory] = React.useState("");
 
   const [open, setOpen] = React.useState(false);
 
@@ -65,45 +70,100 @@ const ProductFilter = () => {
             gap: "1rem",
           }}
         >
-          <FormControl fullWidth>
-            <InputLabel>Category</InputLabel>
-            <Select
-              label="Category"
-              value={selectedCategory}
-              onChange={(event) => {
-                setSelectedCategory(event.target.value);
-              }}
-            >
-              {categories.map((category, index) => {
-                return (
-                  <MenuItem
-                    key={index}
-                    value={category}
-                    sx={{ textTransform: "capitalize" }}
-                  >
-                    {category}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
-          <PriceRangePicker />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="error" variant="contained">
-            Cancel
-          </Button>
-          <Button
-            color="success"
-            variant="contained"
-            onClick={() => {
-              dispatch(setCategory(selectedCategory));
+          <Formik
+            initialValues={{
+              minPrice: 0,
+              maxPrice: 0,
+              category: "",
+            }}
+            validationSchema={Yup.object({
+              category: Yup.string().oneOf(categories),
+              minPrice: Yup.number().min(0, "Price cannot be negative"),
+              maxPrice: Yup.number()
+                .min(0, "Price cannot be negative")
+                .test({
+                  name: "maxPrice",
+                  message: "Max price must be greater than min price.",
+                  test: function (value) {
+                    return value >= this.parent.minPrice;
+                  },
+                }),
+            })}
+            onSubmit={(values) => {
+              if (values.minPrice) {
+                dispatch(setMaxPrice(values?.minPrice));
+              }
+              if (values.maxPrice) {
+                dispatch(setMaxPrice(values?.maxPrice));
+              }
+              if (values.category) {
+                dispatch(setCategory(values?.category));
+              }
               handleClose();
             }}
           >
-            Apply
-          </Button>
-        </DialogActions>
+            {({ handleSubmit, getFieldProps, touched, errors }) => (
+              <form
+                onSubmit={handleSubmit}
+                style={{
+                  display: "flex",
+                  gap: "1rem",
+                  flexDirection: "column",
+                }}
+                name="price-range-picker"
+              >
+                <FormControl fullWidth sx={{ marginTop: "0.3rem" }}>
+                  <InputLabel>Category</InputLabel>
+                  <Select label="Category" {...getFieldProps("category")}>
+                    {categories.map((category, index) => {
+                      return (
+                        <MenuItem
+                          key={index}
+                          value={category}
+                          sx={{ textTransform: "capitalize" }}
+                        >
+                          {category}
+                        </MenuItem>
+                      );
+                    })}
+                  </Select>
+                </FormControl>
+                <FormControl>
+                  <TextField
+                    type="number"
+                    label="Min Price"
+                    {...getFieldProps("minPrice")}
+                  />
+                  {touched.minPrice && errors.minPrice ? (
+                    <FormHelperText error>{errors.minPrice}</FormHelperText>
+                  ) : null}
+                </FormControl>
+                <FormControl>
+                  <TextField
+                    type="number"
+                    label="Max Price"
+                    {...getFieldProps("maxPrice")}
+                  />
+                  {touched.maxPrice && errors.maxPrice ? (
+                    <FormHelperText error>{errors.maxPrice}</FormHelperText>
+                  ) : null}
+                </FormControl>
+                <DialogActions>
+                  <Button
+                    onClick={handleClose}
+                    color="error"
+                    variant="contained"
+                  >
+                    Cancel
+                  </Button>
+                  <Button color="success" variant="contained" type="submit">
+                    Apply
+                  </Button>
+                </DialogActions>
+              </form>
+            )}
+          </Formik>
+        </DialogContent>
       </Dialog>
     </React.Fragment>
   );
