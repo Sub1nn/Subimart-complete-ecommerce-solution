bed0912c5466629491375fea5f29361f5af82eff
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-04-11 22:33:03 +0300

    continueShopping component finished

--- ui/src/components/ContinueShopping.jsx
+++ ui/src/components/ContinueShopping.jsx
@@ -1,22 +1,39 @@
-import { Button, Stack, Typography } from "@mui/material";
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Button, Stack, Typography } from "@mui/material"
+import React from "react"
+import { useNavigate } from "react-router-dom"
 
 const ContinueShopping = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   return (
-    <Stack sx={{ width: "30%", margin: " 20rem auto" }}>
+    <Stack
+      sx={{
+        minWidth: "400px",
+        height: "100px",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "1rem",
+        padding: "5rem",
+        borderRadius: "5px",
+        boxShadow: {
+          xs: "none",
+          md: "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px",
+        },
+      }}
+    >
+      <Typography variant="h5" textAlign="center">
+        No item is added to cart
+      </Typography>
       <Button
-        variant="contained"
         color="secondary"
+        variant="contained"
         onClick={() => {
-          navigate("/product/list");
+          navigate("/product/list")
         }}
       >
         Continue Shopping
       </Button>
     </Stack>
-  );
-};
+  )
+}
 
-export default ContinueShopping;
+export default ContinueShopping
