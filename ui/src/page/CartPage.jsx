018d55748b1d1237f29a85e8646d3e4a341a8648
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-04-11 22:36:12 +0300

    chore: various small fixes

--- ui/src/page/CartPage.jsx
+++ ui/src/page/CartPage.jsx
@@ -1,6 +1,6 @@
 // ... (import statements)
-import React, { useState } from "react";
-import CartItem from "../components/CartItem";
+import React, { useState } from "react"
+import CartItem from "../components/CartItem"
 import {
   Box,
   Button,
@@ -8,45 +8,45 @@ import {
   Typography,
   useMediaQuery,
   useTheme,
-} from "@mui/material";
-import CartCheckout from "../components/CartCheckout";
-import { useMutation, useQuery, useQueryClient } from "react-query";
-import $axios from "../../lib/axios.instance";
-import Loader from "../components/Loader";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+} from "@mui/material"
+import CartCheckout from "../components/CartCheckout"
+import { useMutation, useQuery, useQueryClient } from "react-query"
+import $axios from "../../lib/axios.instance"
+import Loader from "../components/Loader"
+import { useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
 import {
   openErrorSnackbar,
   openSuccessSnackbar,
-} from "../store/slices/snackbar.slice";
-import ContinueShopping from "../components/ContinueShopping";
+} from "../store/slices/snackbar.slice"
+import ContinueShopping from "../components/ContinueShopping"
 
 const CartPage = () => {
-  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const queryClient = useQueryClient();
-  const [orderProductList, setOrderProductList] = useState([]);
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"))
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const queryClient = useQueryClient()
+  const [orderProductList, setOrderProductList] = useState([])
 
   const { isLoading: flushLoading, mutate: flushCart } = useMutation({
     mutationKey: ["flush-cart"],
     mutationFn: async () => {
-      return await $axios.delete("/cart/flush");
+      return await $axios.delete("/cart/flush")
     },
     onSuccess: (response) => {
-      dispatch(openSuccessSnackbar(response?.data?.message));
-      queryClient.invalidateQueries("cart-item-list");
-      queryClient.invalidateQueries("cart-item-count");
+      dispatch(openSuccessSnackbar(response?.data?.message))
+      queryClient.invalidateQueries("cart-item-list")
+      queryClient.invalidateQueries("cart-item-count")
     },
     onError: (error) => {
-      dispatch(openErrorSnackbar(error?.response?.data?.message));
+      dispatch(openErrorSnackbar(error?.response?.data?.message))
     },
-  });
+  })
 
   const { data, isLoading } = useQuery({
-    queryKey: ["cart-list"],
+    queryKey: ["cart-item-list"],
     queryFn: async () => {
-      return await $axios.get("/cart/item/list");
+      return await $axios.get("/cart/item/list")
     },
     onSuccess: (res) => {
       const requiredData = res?.data?.cartData.map((item) => {
@@ -54,21 +54,21 @@ const CartPage = () => {
           productId: item.productId,
 
           orderedQuantity: item.orderedQuantity,
-        };
-      });
+        }
+      })
       // console.log({ requiredData: requiredData });
-      setOrderProductList(requiredData);
+      setOrderProductList(requiredData)
     },
-  });
+  })
 
-  const cartData = data?.data?.cartData;
-  const orderSummary = data?.data?.orderSummary;
+  const cartData = data?.data?.cartData
+  const orderSummary = data?.data?.orderSummary
 
   if (isLoading || flushLoading) {
-    return <Loader />;
+    return <Loader />
   }
   if (cartData?.length === 0) {
-    return <ContinueShopping />;
+    return <ContinueShopping />
   }
 
   return (
@@ -110,7 +110,7 @@ const CartPage = () => {
           )}
           {cartData.length > 0 &&
             cartData.map((item) => {
-              return <CartItem key={item._id} {...item} />;
+              return <CartItem key={item._id} {...item} />
             })}
         </Stack>
         <Stack
@@ -128,7 +128,7 @@ const CartPage = () => {
         </Stack>
       </Box>
     </>
-  );
-};
+  )
+}
 
-export default CartPage;
+export default CartPage
