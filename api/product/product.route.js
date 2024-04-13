f2ddcee3ff1ce5b250e04595c5c4ce946b327614
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-04-13 23:06:42 +0300

    feat: custom hook implemented, useLoggedInUser

--- api/product/product.route.js
+++ api/product/product.route.js
@@ -1,19 +1,19 @@
-import express from "express";
-import { validateReqBody } from "../middleware/validation.middleware.js";
-import { buyerPaginationSchema, productSchema } from "./product.validation.js";
+import express from "express"
+import { validateReqBody } from "../middleware/validation.middleware.js"
+import { buyerPaginationSchema, productSchema } from "./product.validation.js"
 import {
   isBuyer,
   isSeller,
   isUser,
-} from "../middleware/authentication.middleware.js";
-import Product from "./product.model.js";
+} from "../middleware/authentication.middleware.js"
+import Product from "./product.model.js"
 // import mongoose from "mongoose";
-import { checkMongoIdValidity } from "../utils/check.mongo.id.validity.js";
-import { checkProductOwnership } from "../middleware/check.product.ownership.js";
-import { paginationSchema } from "./product.validation.js";
-import Cart from "../cart/cart.model.js";
+import { checkMongoIdValidity } from "../utils/check.mongo.id.validity.js"
+import { checkProductOwnership } from "../middleware/check.product.ownership.js"
+import { paginationSchema } from "./product.validation.js"
+import Cart from "../cart/cart.model.js"
 
-const router = express.Router();
+const router = express.Router()
 
 // add product
 
@@ -23,17 +23,17 @@ router.post(
   validateReqBody(productSchema),
   async (req, res) => {
     //    extract new product from req.body
-    const newProduct = req.body;
+    const newProduct = req.body
 
     // set loggedInUserId as newProduct.ownerId
-    newProduct.ownerId = req.loggedInUser._id;
+    newProduct.ownerId = req.loggedInUser._id
 
     // create product
-    await Product.create(newProduct);
+    await Product.create(newProduct)
 
-    return res.status(200).send({ message: "Product is added successfully." });
+    return res.status(200).send({ message: "Product is added successfully." })
   }
-);
+)
 
 // get a single product details from product :id
 
@@ -43,26 +43,26 @@ router.get(
   checkMongoIdValidity,
   async (req, res) => {
     // extract id from req.params
-    const productId = req.params.id;
+    const productId = req.params.id
 
     // find product and keep in a variable requiredProduct
-    const requiredProduct = await Product.findOne({ _id: productId });
+    const requiredProduct = await Product.findOne({ _id: productId })
 
     // if not product, throw error
 
     if (!requiredProduct) {
-      return res.status(404).send({ message: "Product does not exist." });
+      return res.status(404).send({ message: "Product does not exist." })
     }
 
     //   hide ownerId
-    requiredProduct.ownerId = undefined;
+    requiredProduct.ownerId = undefined
 
     // send product details as response
     return res
       .status(200)
-      .send({ message: "success", product: requiredProduct });
+      .send({ message: "success", product: requiredProduct })
   }
-);
+)
 
 // delete product
 
@@ -73,14 +73,14 @@ router.delete(
   checkProductOwnership,
   async (req, res) => {
     // get id from req.params
-    const productId = req.params.id;
+    const productId = req.params.id
     // delete that one product
-    await Product.deleteOne({ _id: productId });
+    await Product.deleteOne({ _id: productId })
     // remove the product form cart
-    await Cart.deleteMany({ productId: productId });
-    return res.status(200).send({ message: "Product is deleted successfully" });
+    await Cart.deleteMany({ productId: productId })
+    return res.status(200).send({ message: "Product is deleted successfully" })
   }
-);
+)
 
 // edit a product
 
@@ -92,15 +92,15 @@ router.put(
   validateReqBody(productSchema),
   async (req, res) => {
     // get id from req.params
-    const { id } = req.params;
+    const { id } = req.params
     // get new values from req.body
-    const newValues = req.body;
+    const newValues = req.body
     // update product
-    await Product.updateOne({ _id: id }, { $set: { ...newValues } });
+    await Product.updateOne({ _id: id }, { $set: { ...newValues } })
     // send response with message
-    return res.status(200).send({ message: "Product is updated successfully" });
+    return res.status(200).send({ message: "Product is updated successfully" })
   }
-);
+)
 
 // get all product list by buyer
 
@@ -109,29 +109,23 @@ router.post(
   isBuyer,
   validateReqBody(buyerPaginationSchema),
   async (req, res) => {
-    console.log(req.body);
-    //   const { page, limit } = req.query;
-    //   const skip = limit * (page - 1);
-    //   const products = await Product.find({ ownerId: req.loggedInUser._id })
-    //  .skip(skip)
-    //  .limit(limit);
-    //   return res.status(200).send({ products });
+    // console.log(req.body)
 
     // extract pagination data from req.body
-    const { page, limit, searchText, category, minPrice, maxPrice } = req.body;
+    const { page, limit, searchText, category, minPrice, maxPrice } = req.body
     // calculate skip
-    const skip = (page - 1) * limit;
+    const skip = (page - 1) * limit
 
     // filter stage
 
-    let match = {};
+    let match = {}
 
     if (searchText) {
-      match = { name: { $regex: searchText, $options: "i" } };
+      match = { name: { $regex: searchText, $options: "i" } }
     }
 
     if (category) {
-      match = { ...match, category: category };
+      match = { ...match, category: category }
     }
 
     if (minPrice || maxPrice) {
@@ -141,7 +135,7 @@ router.post(
           ...(minPrice ? { $gte: minPrice } : {}),
           ...(maxPrice ? { $lte: maxPrice } : {}),
         },
-      };
+      }
     }
 
     //create the query
@@ -165,19 +159,19 @@ router.post(
           description: { $substr: ["$description", 0, 200] },
         },
       },
-    ]);
+    ])
 
     // calculate number of page
-    const totalProduct = await Product.find(match).countDocuments();
-    const numberOfPages = Math.ceil(totalProduct / limit);
+    const totalProduct = await Product.find(match).countDocuments()
+    const numberOfPages = Math.ceil(totalProduct / limit)
 
     return res.status(200).send({
       message: "success",
       products: products,
       numberOfPages: numberOfPages,
-    });
+    })
   }
-);
+)
 
 // get all product list by seller
 router.post(
@@ -186,19 +180,19 @@ router.post(
   validateReqBody(paginationSchema),
   async (req, res) => {
     // extract pagination data from req.body
-    const { page, limit, searchText } = req.body;
+    const { page, limit, searchText } = req.body
 
     // calculate skip
-    const skip = (page - 1) * limit;
+    const skip = (page - 1) * limit
 
     // filter stage
-    let match = { ownerId: req.loggedInUserId };
+    let match = { ownerId: req.loggedInUserId }
 
     if (searchText) {
       match = {
         ownerId: req.loggedInUserId,
         name: { $regex: searchText, $options: "i" },
-      };
+      }
     }
 
     let products = await Product.aggregate([
@@ -223,37 +217,38 @@ router.post(
           description: { $substr: ["$description", 0, 200] },
         },
       },
-    ]);
+    ])
 
     // calculate number of page
-    const totalProduct = await Product.find(match).countDocuments();
-    const numberOfPages = Math.ceil(totalProduct / limit);
+    const totalProduct = await Product.find(match).countDocuments()
+    const numberOfPages = Math.ceil(totalProduct / limit)
 
     return res.status(200).send({
       message: "success",
       products: products,
       numberOfPages: numberOfPages,
-    });
+    })
   }
-);
+)
 
 // get all products for carousel
 
 router.get("/product/carousel/list", async (req, res) => {
-  const products = await Product.find();
+  const products = await Product.find()
 
   const carouselProducts = products.map((product) => ({
+    id: product.id,
     name: product.name,
     price: product.price,
     brand: product.brand,
     image: product.image,
     description: product.description.substring(0, 200),
-  }));
+  }))
 
   return res.status(200).send({
     message: "success",
     products: carouselProducts,
-  });
-});
+  })
+})
 
-export default router;
+export default router
--- ui/src/custom-hooks/useLoggedInUser.js
+++ ui/src/custom-hooks/useLoggedInUser.js
@@ -1,4 +1,4 @@
-export const useLoggedInUser = () => {
+const useLoggedInUser = () => {
   const token = localStorage.getItem("token")
 
   if (token) {
