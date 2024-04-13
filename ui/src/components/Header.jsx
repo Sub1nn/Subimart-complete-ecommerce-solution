1d531657ce71d907b5196cd35531660d02bc5bd0
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-04-13 23:10:47 +0300

    style: layout fixed, greeting component implemented

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -1,29 +1,30 @@
-import MenuIcon from "@mui/icons-material/Menu";
-import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import { Badge, ThemeProvider, createTheme } from "@mui/material";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import * as React from "react";
-import { useQuery } from "react-query";
-import { useNavigate } from "react-router-dom";
-import $axios from "../../lib/axios.instance";
-import LogoutDialog from "./LogoutDialog";
-import ThemeToggle from "./ThemeToggle";
-import { useDispatch } from "react-redux";
-import { clearFilter } from "../store/slices/productSlice";
+import MenuIcon from "@mui/icons-material/Menu"
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined"
+import { Badge, ThemeProvider, createTheme } from "@mui/material"
+import AppBar from "@mui/material/AppBar"
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+import CssBaseline from "@mui/material/CssBaseline"
+import Divider from "@mui/material/Divider"
+import Drawer from "@mui/material/Drawer"
+import IconButton from "@mui/material/IconButton"
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemText from "@mui/material/ListItemText"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import * as React from "react"
+import { useQuery } from "react-query"
+import { useNavigate } from "react-router-dom"
+import $axios from "../../lib/axios.instance"
+import LogoutDialog from "./LogoutDialog"
+import ThemeToggle from "./ThemeToggle"
+import { useDispatch } from "react-redux"
+import { clearFilter } from "../store/slices/productSlice"
+import GreetingPopover from "./GreetingPopover"
 
-const drawerWidth = 240;
+const drawerWidth = 240
 const navItems = [
   {
     id: 1,
@@ -36,44 +37,44 @@ const navItems = [
     path: "/product/list",
   },
   {
-    id: 1,
+    id: 3,
     name: "About",
     path: "/about",
   },
-];
+]
 
-const lightTheme = createTheme();
+const lightTheme = createTheme()
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
   },
-});
+})
 
 const Header = (props) => {
-  const dispatch = useDispatch();
-  const [theme, setTheme] = React.useState("light");
+  const dispatch = useDispatch()
+  const [theme, setTheme] = React.useState("light")
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-  };
-  const userRole = localStorage.getItem("role");
-  const navigate = useNavigate();
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"))
+  }
+  const userRole = localStorage.getItem("role")
+  const navigate = useNavigate()
+  const { window } = props
+  const [mobileOpen, setMobileOpen] = React.useState(false)
 
   // get cart count
   const { data, isLoading } = useQuery({
     queryKey: ["cart-item-count"],
     queryFn: async () => {
-      return await $axios.get("/cart/item/count");
+      return await $axios.get("/cart/item/count")
     },
     enabled: userRole === "buyer",
-  });
+  })
 
-  const cartItemCount = data?.data?.cartItemCount;
+  const cartItemCount = data?.data?.cartItemCount
 
   const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
+    setMobileOpen((prevState) => !prevState)
+  }
 
   const drawer = (
     <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
@@ -87,7 +88,7 @@ const Header = (props) => {
             key={item.id}
             disablePadding
             onClick={() => {
-              navigate(item.path);
+              navigate(item.path)
             }}
           >
             <ListItemButton sx={{ textAlign: "center" }}>
@@ -97,62 +98,69 @@ const Header = (props) => {
         ))}
       </List>
     </Box>
-  );
+  )
 
   const container =
-    window !== undefined ? () => window().document.body : undefined;
+    window !== undefined ? () => window().document.body : undefined
 
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <Box sx={{ display: "flex" }}>
         <CssBaseline />
         <AppBar component="nav" sx={{ backgroundColor: "#5D3587" }}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: "none" } }}
+          <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: "none" } }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{
+                  display: { xs: "block", md: "inline-block" }, // Display inline-block on medium and larger screens
+                  width: { xs: "100%", md: "auto" }, // Adjust width for medium and larger screens
+                  whiteSpace: "nowrap", // Prevent text from wrapping
+                }}
+              >
+                Subi Mart
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: "1rem",
+                width: { xs: "100%", sm: "auto" },
+              }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-            >
-              Subi Mart
-            </Typography>
-            <ThemeToggle toggleTheme={toggleTheme} theme={theme} />
-            <Box sx={{ display: { xs: "none", sm: "block" } }}>
               {navItems.map((item) => (
                 <Button
                   key={item.id}
-                  sx={{ color: "#fff" }}
+                  sx={{
+                    color: "#fff",
+                    display: { xs: "none", sm: "inline-block" },
+                  }} // Hide on mobile, show on larger screens
                   onClick={() => {
-                    navigate(item.path);
-                    dispatch(clearFilter());
+                    navigate(item.path)
+                    dispatch(clearFilter())
                   }}
                 >
                   {item.name}
                 </Button>
               ))}
             </Box>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                gap: "1rem",
-                marginLeft: "3rem",
-              }}
-            >
+            <Box sx={{ display: "flex", alignItems: "center", gap: "1rem" }}>
               {userRole === "buyer" && (
                 <IconButton
                   size="large"
                   onClick={() => {
-                    navigate("/cart");
+                    navigate("/cart")
                   }}
                 >
                   <Badge
@@ -164,7 +172,7 @@ const Header = (props) => {
                   </Badge>
                 </IconButton>
               )}
-              <LogoutDialog />
+              <GreetingPopover />
             </Box>
           </Toolbar>
         </AppBar>
@@ -190,7 +198,7 @@ const Header = (props) => {
         </nav>
       </Box>
     </ThemeProvider>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
