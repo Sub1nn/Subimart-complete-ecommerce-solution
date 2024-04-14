93be12e6ba2919f62193722d4b8f210dd19a13cc
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-04-14 23:36:35 +0300

    feat: implemented mui carousel instead of react responsive carousel

--- ui/src/page/Home.jsx
+++ ui/src/page/Home.jsx
@@ -1,66 +1,36 @@
-import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Link, useParams } from "react-router-dom";
-import "./home.css";
-import { useQuery } from "react-query";
-import $axios from "../../lib/axios.instance";
-import Loader from "../components/Loader";
-import { Grid, Stack, Typography } from "@mui/material";
-import ProductCard from "../components/ProductCard";
+import React from "react"
+// import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Link, useNavigate, useParams } from "react-router-dom"
+import "./home.css"
+import { useQuery } from "react-query"
+import $axios from "../../lib/axios.instance"
+import Loader from "../components/Loader"
+import { Grid, Stack, Typography } from "@mui/material"
+import ProductCard from "../components/ProductCard"
+import SlidingCarousel from "../components/Carousel"
 
 const Home = () => {
-  const { id } = useParams();
+  const navigate = useNavigate()
+
   const { isLoading, data } = useQuery({
     queryKey: ["product-carousel"],
     queryFn: async () => {
-      return await $axios.get("/product/carousel/list");
+      return await $axios.get("/product/carousel/list")
     },
-  });
-
-  const productData = data?.data?.products;
-  console.log(productData);
-
+  })
+  const productData = data?.data?.products
   if (isLoading) {
-    return <Loader />;
+    return <Loader />
   }
-
   // Limiting popular products to 6 items
-  const popularProducts = productData.slice(0, 6);
+  // Ensure productData is not empty before slicing
+  const popularProducts =
+    productData && productData.length > 0 ? productData.slice(0, 6) : []
+
   return (
     <>
-      <div className="poster">
-        <Carousel
-          showThumbs={false}
-          autoPlay={true}
-          transitionTime={1}
-          infiniteLoop={true}
-          showStatus={false}
-        >
-          {productData.map((product) => (
-            <Link
-              style={{ textDecoration: "none", color: "white" }}
-              to={`/product/details/${product.id}`}
-              key={product.id}
-            >
-              <div className="posterImage">
-                <img src={product.image} alt={product.name} />
-              </div>
-              <div className="posterImage__overlay">
-                <div className="posterImage__title">
-                  {product ? product.name : ""}
-                </div>
-                <div className="posterImage__brand">
-                  {product ? product.brand : ""}
-                </div>
-                <div className="posterImage__description">
-                  {product ? product.description : ""}
-                </div>
-              </div>
-            </Link>
-          ))}
-        </Carousel>
-      </div>
+      <SlidingCarousel />
       <div
         style={{
           marginTop: "3rem",
@@ -74,15 +44,22 @@ const Home = () => {
       </div>
       <Grid container spacing={4} justifyContent="center" mt={"1px"}>
         {popularProducts.map((item) => {
-          return (
-            <Grid key={item._id} item xs={12} sm={6} md={4} lg={3}>
-              <ProductCard {...item} />
-            </Grid>
-          );
+          if (item && item.id) {
+            return (
+              <Grid key={item.id} item xs={12} sm={6} md={4} lg={3}>
+                <ProductCard
+                  {...item}
+                  onClick={() => navigate(`/product/detail/${item.id}`)}
+                />
+              </Grid>
+            )
+          } else {
+            return null
+          }
         })}
       </Grid>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
