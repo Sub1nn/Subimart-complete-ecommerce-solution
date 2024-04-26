c5b6a356f3242adf05a7eb4d21767e07929cffa0
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-04-26 11:35:51 +0300

    minor bug fixes

--- api/index.js
+++ api/index.js
@@ -1,33 +1,33 @@
-import express from "express";
-import userRoutes from "./user/user.route.js";
-import { connectDb } from "./connect.db.js";
-import productRoutes from "./product/product.route.js";
-import cartRoutes from "./cart/cart.route.js";
-import orderRoutes from "./order/order.route.js";
-import paymentRoutes from "./payment/payment.route.js";
-import cors from "cors";
+import express from "express"
+import userRoutes from "./user/user.route.js"
+import { connectDb } from "./connect.db.js"
+import productRoutes from "./product/product.route.js"
+import cartRoutes from "./cart/cart.route.js"
+import orderRoutes from "./order/order.route.js"
+import paymentRoutes from "./payment/payment.route.js"
+import cors from "cors"
 
-const app = express();
+const app = express()
 
 // to make app understand json
-app.use(express.json());
+app.use(express.json())
 
 //cors
-app.use(cors());
+app.use(cors())
 
 // connect database
-connectDb();
+connectDb()
 
 // register routes
-app.use(userRoutes);
-app.use(productRoutes);
-app.use(cartRoutes);
-app.use(paymentRoutes);
-app.use(orderRoutes);
+app.use(userRoutes)
+app.use(productRoutes)
+app.use(cartRoutes)
+app.use(paymentRoutes)
+app.use(orderRoutes)
 
 // port
-const PORT = 3000;
+const PORT = 4000
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
