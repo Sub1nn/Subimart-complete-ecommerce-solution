3ffdd7074b2bfe8f2c105ca957187ae2cfa58cc5
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-04-26 11:31:18 +0300

    baseURL updated

--- ui/lib/axios.instance.js
+++ ui/lib/axios.instance.js
@@ -1,21 +1,21 @@
-import axios from "axios";
+import axios from "axios"
 
 const $axios = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:4000",
   timeout: 5000,
-});
+})
 
 // Add a request interceptor
 
 $axios.interceptors.request.use(function (config) {
   // Do something before request is sent
-  const accessToken = localStorage.getItem("token");
+  const accessToken = localStorage.getItem("token")
 
   if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
+    config.headers.Authorization = `Bearer ${accessToken}`
   }
 
-  return config;
-});
+  return config
+})
 
-export default $axios;
+export default $axios
