0ade471ede6f4f3c7e806b37bd0c357523b2cfeb
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-04-26 11:36:43 +0300

    authGuard for guarding routes

--- ui/src/guards/AuthGuard.jsx
+++ ui/src/guards/AuthGuard.jsx
@@ -1,11 +1,18 @@
-import React from "react"
+import React, { useEffect } from "react"
 import useLoggedInUser from "../custom-hooks/useLoggedInUser"
+import { useNavigate } from "react-router-dom"
 
 const AuthGuard = (props) => {
   const isLoggedIn = useLoggedInUser
-  console.log(isLoggedIn)
+  const navigate = useNavigate()
 
-  return <div>{props.children}</div>
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/login")
+    }
+  }, [isLoggedIn, navigate])
+
+  return <>{isLoggedIn && props.children}</>
 }
 
 export default AuthGuard
