d09085c63273013005da78b4b171d58a0794b838
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-04-26 11:37:09 +0300

    GuestGuard for guarding guest routes

--- ui/src/guards/GuestGuard.jsx
+++ ui/src/guards/GuestGuard.jsx
@@ -1,7 +1,24 @@
 import React from "react"
+import useLoggedInUser from "../custom-hooks/useLoggedInUser"
+import { useEffect } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
 
 const GuestGuard = (props) => {
-  return <div>{props.children}</div>
+  const isLoggedIn = useLoggedInUser()
+  const navigate = useNavigate()
+
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/home")
+    }
+    if (pathname === "/" && !isLoggedIn) {
+      navigate("/login")
+    }
+  }, [isLoggedIn, navigate, pathname])
+
+  return <>{!isLoggedIn && props.children}</>
 }
 
 export default GuestGuard
