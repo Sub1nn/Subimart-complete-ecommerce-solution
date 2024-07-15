fae382ca92e66e90cf9385d3e4ac916fa6ef061f
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-07-15 22:50:45 +0300

    style: improved layout orientation

--- ui/src/layout/MainLayout.jsx
+++ ui/src/layout/MainLayout.jsx
@@ -1,35 +1,35 @@
-import React from "react"
-import { Outlet } from "react-router-dom"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import CustomSnackbar from "../components/CustomSnackbar"
-import { Box, Stack } from "@mui/material"
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import CustomSnackbar from "../components/CustomSnackbar";
+import { Box, Stack } from "@mui/material";
 
 const MainLayout = () => {
   return (
     <>
       <CustomSnackbar />
-      <Stack spacing={7} sx={{ margin: 0 }}>
+      <Stack spacing={6}>
         <Header />
-
-        <Box
-          sx={{
-            padding: "2rem",
-            display: "flex",
-            width: "100%",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            minHeight: "80vh",
-          }}
-        >
-          <Outlet />
-        </Box>
-
-        <Footer />
+        <Stack spacing={2} sx={{ width: "100%" }}>
+          <Box
+            sx={{
+              padding: "2rem",
+              display: "flex",
+              width: "100%",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              minHeight: "80vh",
+            }}
+          >
+            <Outlet />
+          </Box>
+          <Footer />
+        </Stack>
       </Stack>
     </>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
