3a9070727ed73503bd5e3a45daa179251feb9f58
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-07-15 22:51:50 +0300

    fix: product data fetched as props

--- ui/src/page/BuyerProductList.jsx
+++ ui/src/page/BuyerProductList.jsx
@@ -1,53 +1,8 @@
-import React, { useState } from "react"
-import Footer from "../components/Footer"
-import Header from "../components/Header"
-import ProductCard from "../components/ProductCard"
-import { useQuery } from "react-query"
-import $axios from "../../lib/axios.instance"
-import {
-  Box,
-  CircularProgress,
-  Grid,
-  Pagination,
-  Stack,
-  TextField,
-} from "@mui/material"
-import Loader from "../components/Loader"
-import AccountCircle from "@mui/icons-material/AccountCircle"
-import { useSelector } from "react-redux"
-import NoProductFound from "../components/NoProductFound"
+import { Grid } from "@mui/material";
+import React from "react";
+import ProductCard from "../components/ProductCard";
 
-const BuyerProductList = () => {
-  const [page, setPage] = useState(1)
-
-  const { searchText, category, minPrice, maxPrice, isFilterApplied } =
-    useSelector((state) => state.product)
-
-  const { isLoading, isError, data, error } = useQuery({
-    queryKey: ["buyer-product-list", page, searchText, category],
-    queryFn: async () => {
-      return await $axios.post("/product/buyer/list", {
-        page: page,
-        limit: 3,
-        searchText: searchText,
-        category: category || null,
-        minPrice: minPrice || null,
-        maxPrice: maxPrice || null,
-        isFilterApplied: isFilterApplied,
-      })
-    },
-  })
-  console.log(data)
-  const productData = data?.data?.products
-  const totalPages = data?.data?.numberOfPages
-  // console.log(productData)
-
-  if (isLoading) {
-    return <Loader />
-  }
-  if (productData.length === 0) {
-    return <NoProductFound />
-  }
+const BuyerProductList = ({ productData }) => {
   return (
     <>
       <Grid container spacing={4} justifyContent="center">
@@ -56,20 +11,11 @@ const BuyerProductList = () => {
             <Grid key={item._id} item xs={12} sm={6} md={4} lg={3}>
               <ProductCard {...item} />
             </Grid>
-          )
+          );
         })}
       </Grid>
-      <Pagination
-        sx={{ display: "flex", justifyContent: "center", margin: "2rem 0" }}
-        page={page}
-        count={totalPages}
-        color="secondary"
-        onChange={(event, value) => {
-          setPage(value)
-        }}
-      />
     </>
-  )
-}
+  );
+};
 
-export default BuyerProductList
+export default BuyerProductList;
--- ui/src/page/SellerProductList.jsx
+++ ui/src/page/SellerProductList.jsx
@@ -1,55 +1,21 @@
-import { Button, CircularProgress, Grid, Stack } from "@mui/material"
-import React from "react"
-import { useQuery } from "react-query"
-import { useNavigate } from "react-router-dom"
-import $axios from "../../lib/axios.instance"
-import ProductCard from "../components/ProductCard"
-import Loader from "../components/Loader"
-import { useSelector } from "react-redux"
+import { Grid } from "@mui/material";
+import React from "react";
+import ProductCard from "../components/ProductCard";
 
-const SellerProductList = () => {
-  const navigate = useNavigate()
-
-  const { searchText, category, minPrice, maxPrice, isFilterApplied } =
-    useSelector((state) => state.product)
-
-  const { error, data, isLoading, isError } = useQuery({
-    queryKey: ["seller-product-list", searchText, category],
-    queryFn: () => {
-      return $axios.post("/product/seller/list", {
-        page: 1,
-        limit: 10,
-        searchText: searchText,
-        category: category || null,
-        minPrice: minPrice || null,
-        maxPrice: maxPrice || null,
-        isFilterApplied: isFilterApplied,
-      })
-    },
-    onError: (error) => {
-      console.log(error)
-    },
-  })
-
-  const productList = data?.data?.products
-
-  if (isLoading) {
-    return <Loader />
-  }
-
+const SellerProductList = ({ productData }) => {
   return (
     <>
       <Grid container spacing={4} justifyContent="center">
-        {productList.map((item) => {
+        {productData.map((item) => {
           return (
             <Grid key={item._id} item xs={12} sm={6} md={4} lg={3}>
               <ProductCard {...item} />
             </Grid>
-          )
+          );
         })}
       </Grid>
     </>
-  )
-}
+  );
+};
 
-export default SellerProductList
+export default SellerProductList;
