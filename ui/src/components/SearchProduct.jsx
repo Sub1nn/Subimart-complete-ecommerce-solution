658baf674f9b29eee11a3feee9cd653e71eb3a83
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-07-15 22:50:10 +0300

    feat: focus implemented in search box with useref

--- ui/src/components/SearchProduct.jsx
+++ ui/src/components/SearchProduct.jsx
@@ -1,41 +1,65 @@
-import { FormControl, Input, InputAdornment } from "@mui/material"
-import React, { useState } from "react"
-import SearchIcon from "@mui/icons-material/Search"
-import { useDispatch } from "react-redux"
-import { setSearchText } from "../store/slices/productSlice"
-import useDebounce from "../../Hooks/useDebounce"
+import { FormControl, Input, InputAdornment } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import { useDispatch, useSelector } from "react-redux";
+import { clearFilter, setSearchText } from "../store/slices/productSlice";
+import useDebounce from "../../Hooks/useDebounce";
 
 const SearchProduct = () => {
-  const dispatch = useDispatch()
-  const [searchInput, setSearchInput] = useState("")
+  const dispatch = useDispatch();
+  const searchText = useSelector((state) => state.product.searchText);
+  const [searchInput, setSearchInput] = useState(searchText || "");
+  const inputRef = useRef(null);
 
   // Use debounce hook with 500ms delay
-  const debouncedSearchInput = useDebounce(searchInput, 500)
+  const debouncedSearchInput = useDebounce(searchInput, 500);
 
   const handleInputChange = (event) => {
-    setSearchInput(event.target.value)
-  }
+    const value = event.target.value;
+    setSearchInput(value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Backspace" && searchInput === "") {
+      dispatch(clearFilter());
+    }
+  };
 
   // Dispatch setSearchText with debounced value
-  React.useEffect(() => {
-    // console.log(debouncedSearchInput);
-    dispatch(setSearchText(debouncedSearchInput))
-  }, [debouncedSearchInput, dispatch])
+  useEffect(() => {
+    if (debouncedSearchInput !== null && debouncedSearchInput.trim() !== "") {
+      dispatch(setSearchText(debouncedSearchInput));
+    } else {
+      dispatch(clearFilter());
+    }
+  }, [debouncedSearchInput, dispatch]);
+
+  // Effect to focus input whenever searchInput changes
+  useEffect(() => {
+    if (inputRef.current && document.activeElement !== inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [searchInput]);
+
+  // reset searchInput state when searchText changes
+  useEffect(() => {
+    setSearchInput(searchText || "");
+  }, [searchText]);
 
   return (
-    <FormControl variant="standard">
-      <Input
-        value={searchInput}
-        placeholder="search"
-        onChange={handleInputChange}
-        startAdornment={
-          <InputAdornment position="start">
-            <SearchIcon color="success" />
-          </InputAdornment>
-        }
-      />
-    </FormControl>
-  )
-}
+    <Input
+      ref={inputRef}
+      value={searchInput}
+      placeholder="Search"
+      onChange={handleInputChange}
+      onKeyDown={handleKeyPress}
+      startAdornment={
+        <InputAdornment position="start">
+          <SearchIcon color="success" />
+        </InputAdornment>
+      }
+    />
+  );
+};
 
-export default SearchProduct
+export default SearchProduct;
